41f585202ea22a65cfe0cc031077de3a2603db81
itmightbeme <beccatrafny@gmail.com>
2025-03-09 14:16:05 -0600

    add story 3 files not ready

---
+++ Portfolio/story.js
@@ -0,0 +1,20 @@
+const greeting = document.getElementById("greeting");
+greeting.style.textAlign = "center";
+greeting.style.marginTop = "50px";
+const username = window.prompt("What is your name", "NAME");
+
+greeting.textContent = "Welome to my website, " + username + "!";
+
+
+const name = document.getElementById("name")
+const gender = document.getElementById("gender")
+const place = document.getElementById("place")
+const dow = document.getElementById("dow")
+const friend = document.getElementById("friend")
+const activity = document.getElementById("activity")
+const food = document.getElementById("food")
+const treat = document.getElementById("treat")
+const introextro = document.getElementById("introextro")
+const number = document.getElementById("number")
+
+
